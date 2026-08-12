099d6af17c759e20923d8611f05e45db668cf3ce
mamadousady <130261009+mamadousady@users.noreply.github.com>
2026-08-12 19:25:11 -0400

    Add production URL to CORS configuration

--- Backends/server.js
+++ Backends/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174','https://ambiance-frontend.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
